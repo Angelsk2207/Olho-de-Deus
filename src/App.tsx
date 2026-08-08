bfb93146292a9428c079a59283904803f0431642
Angelsk2207 <angelsakuraag@gmail.com>
2026-08-08 17:42:38 -0300

    feat: autonomous public OSINT capture pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
 import React from 'react';
 import LeadDashboard from './components/LeadDashboard';
-
-export default function App() {
-  // Modo de teste aberto: sem Google, pop-ups ou login.
-  // O acesso privado será acrescentado depois no painel de administrador.
-  return <LeadDashboard token="" userEmail="Modo de teste" />;
-}
+export default function App() { return <LeadDashboard />; }
